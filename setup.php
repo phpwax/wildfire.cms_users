1258c733e97b03d63f13ad5e562a8219e800c7bd
Charles Marshall <charles@oneblackbear.com>
2013-02-06 16:39:09 +0000

    forgot to register the paths

--- setup.php
+++ setup.php
@@ -1,3 +1,10 @@
 <?
+
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
+AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
+AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
+AutoLoader::$plugin_array[] = array("name"=>"wildfire.cms_users","dir"=>__DIR__);
+
 CMSApplication::register_module("users", array("display_name"=>"Users", "link"=>"/admin/users/"));
+
 ?>
