7f5b9b708eb39716facb9324e0d359a850622ba0
Michal Noskovic <michal@oneblackbear.com>
2018-07-18 19:24:58 +0100

    move these up

--- ini.php
+++ ini.php
@@ -1,3 +1,8 @@
 <?
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
+AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
+AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
+AutoLoader::$plugin_array[] = array("name"=>"wildfire.cms_users","dir"=>__DIR__);
+
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
 ?>
